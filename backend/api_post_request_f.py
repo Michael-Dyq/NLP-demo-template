76c1ad6693adc052d00711e8610ea82fe74a98f0
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2020-10-01 18:42:33 -0400

    POST + JSON/String input adjustments

---
+++ backend/api_post_request_f.py
@@ -0,0 +1,37 @@
+'''
+This script serves to call an API request on the Multilingual NER Demo on port 4033 
+'''
+
+import requests
+import json
+
+# The target URL where we send to request to
+url_pos = 'http://localhost:8081/pos'
+url_ner = 'http://dickens.seas.upenn.edu:4033/ner'
+
+# The parameters we wish to send
+json_pos = {
+        'text' : 'Barack Obama is an American politician and attorney who served as the 44th president of the United States from 2009 to 2017.'
+        }
+
+json_ner = {
+        'lang': 'eng',
+        'model': 'bert',
+        'text' : 'Barack Obama is an American politician and attorney who served as the 44th president of the United States from 2009 to 2017.'
+        }
+# Set the headers for the request
+headers = {'content-type': 'application/json'}
+
+# Post the request
+output_pos = requests.post(url_pos, data = json.dumps(json_pos), headers=headers)
+output_ner = requests.post(url_ner, data = json.dumps(json_ner), headers=headers)
+
+# Merge the output by their service tags
+res = {
+    "pos_anns" : output_pos.json(),
+    "ner_anns" : output_ner.json()
+}
+
+
+# Print the response text (the content of the requested file):
+print(output_pos.json())
--- backend/backend_cherry.py
+++ backend/backend_cherry.py
@@ -1,6 +1,7 @@
 import os.path
 import time
 import json
+from typing import Text
 import cherrypy
 import spacy
 
@@ -18,8 +19,11 @@ else:
     print("model loaded")
 
 # Write a function that takes an input(string/JSON) and returns a JSON output
-def outputToJSON(string):
+def outputToJSON(data):
     """Converting the output of your annotation service to JSON. In this case, our service is POS Tagging"""
+
+    string = data['text']
+    
     # Annotate the input
     docs = model(string)
 
@@ -30,7 +34,6 @@ def outputToJSON(string):
         annotation_res[token.text] = token.pos_
 
     json_output = {
-        "service": "spacy-pos",
         "result": annotation_res
     }
 
@@ -39,40 +42,13 @@ def outputToJSON(string):
 class Annotation(object):
     @cherrypy.expose
     def index(self):
-        return '''
-                <form action="anns" method="GET">
-                Text:
-                <input type="text" name="text" size="50"/>
-                <input type="submit" />
-                </form>
-               '''
+        print("Currently not available")
+        return 'Currently not available'
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def anns(self, text=None):
-        cherrypy.response.headers['Content-Type'] = 'application/json'
-        # CherryPy passes all GET and POST variables as method parameters.
-        # It doesn't make a difference where the variables come from, how
-        # large their contents are, and so on.
-        #
-        # You can define default parameter values as usual. In this
-        # example, the "name" parameter defaults to None so we can check
-        # if a name was actually specified.
-
-        if text:
-            #if the input is a string
-            print("Result: %s" %outputToJSON(text))
-            return outputToJSON(text)
-
-        else:
-            if text is None:
-                # No text is specified
-                return 'Please enter your text <a href="./">here</a>.'
-
-    @cherrypy.expose
     @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def json(self):
+    def pos(self):
         # CherryPy passes all GET and POST variables as method parameters.
         # It doesn't make a difference where the variables come from, how
         # large their contents are, and so on.
@@ -80,14 +56,27 @@ class Annotation(object):
         # You can define default parameter values as usual. In this
         # example, the "name" parameter defaults to None so we can check
         # if a name was actually specified.
-        
         cherrypy.response.headers['Content-Type'] = 'application/json'
-        data = cherrypy.request.json
 
-        if data:
-            #if the input is passed in a JSON document
-            print("Result: %s" %outputToJSON(data["text"]))
-            return outputToJSON(data["text"])
+        try:
+            data = cherrypy.request.json
+            useJSON = True
+            print("\nReading JSON Docs from Request")
+
+        except:
+            data = cherrypy.request.params
+            useJSON=False
+            print("\nReading Parameters from the URL")
+
+        if useJSON:
+            para = cherrypy.request.params
+            if len(para) != 0:
+                print("\nOverwrite JSON with Parameters (HTTP is priority)")
+                data = para
+       
+        result = outputToJSON(data) 
+        print(result)
+        return result
 
 if __name__ == '__main__':
     
